3d7f5926829e4b159d885781126a5b7269ea8122
thanh.huynh <thanh.huynh@ubitec.com>
2022-04-25 02:08:17 +0700

    Fix wrong logic of finding matched users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
-import { InjectRepository } from '@nestjs/typeorm';
 import { CreateActionDto } from './dto/create-like-action.dto';
 import { UsersActionsRepository } from './users-actions.repository';
 import { In } from 'typeorm';
-import { uniq, uniqBy } from 'lodash';
+import { intersection } from 'lodash';
 
 @Injectable()
 export class UsersService {
@@ -36,7 +35,7 @@ export class UsersService {
     const stuffUsers = await this.usersActionsRepository.find({ where: { stuffId: userId, action: "like" } }) || [];
 
     if (likedUsers.length > 0 && stuffUsers.length > 0) {
-      const stuffusersIds = uniq([...likedUsers.map(user => user.stuffId), ...stuffUsers.map(user => user.userId)]);
+      const stuffusersIds = intersection([...likedUsers.map(user => user.stuffId)], [...stuffUsers.map(user => user.userId)]);
       if (stuffusersIds.length > 0) {
         return await this.usersRepository.find({ where: { id: In(stuffusersIds) } });
       }
